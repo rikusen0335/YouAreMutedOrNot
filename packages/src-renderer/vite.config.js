e722c6e3ac0cf59ac7445145ee27c3d80f3d8890
rikusen0335 <rikusen0335@gmail.com>
2021-08-23 06:51:15 +0900

    preact追加

--- packages/src-renderer/vite.config.js
+++ packages/src-renderer/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import preactRefresh from '@prefresh/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
+  plugins: [preactRefresh()],
+  esbuild: {
+    jsxFactory: 'h'
+  },
   resolve: {
     alias: {
-      '~': resolve(__dirname, './'),
-      '@src': resolve(__dirname, 'src')
+      '@': resolve(__dirname, 'src')
     }
   }
 })
