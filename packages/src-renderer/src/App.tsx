a4264712049f7402ce066d19b99cd94e18e4a26c
rikusen0335 <rikusen0335@gmail.com>
2021-08-23 20:36:23 +0900

    LockContext追加

--- packages/src-renderer/src/App.tsx
+++ packages/src-renderer/src/App.tsx
@@ -1,18 +1,18 @@
 import { h } from 'preact'
 import { WindowTitlebar } from './components/WindowTitlebar'
-import { LockContext } from './contexts/LockContext'
-import { useContext, useEffect } from 'preact/hooks'
+import { LockContext, LockProvider } from './contexts/LockContext'
+import { useContext } from 'preact/hooks'
 
 import { WindowManager } from '@tauri-apps/api/window'
 
 export const App = () => {
   return (
-    <LockContext.Provider value={{ isLock: false }} children={<Home />} />
+    <LockProvider children={<Home />} />
   )
 }
 
 const Home = () => {
-  const { isLock } = useContext(LockContext)
+  const { isLock, setIsLock } = useContext(LockContext)
 
   const appWindow: WindowManager = window.__TAURI__.window.appWindow
   appWindow.setResizable(true)
@@ -21,7 +21,8 @@ const Home = () => {
     <WindowTitlebar />,
     <div style={{ marginTop: 30 }}>
       <p style={{ color: '#fff' }}>aiueo</p>
-      <p>{isLock}</p>
+      <p>{isLock && 'Locked'}</p>
+      <button onClick={() => setIsLock(state => !state)}>Change lock</button>
     </div>
   ])
 }
--- packages/src-renderer/src/contexts/LockContext.tsx
+++ packages/src-renderer/src/contexts/LockContext.tsx
@@ -1,7 +1,16 @@
-import { createContext } from 'preact'
+import { h, createContext, ComponentChildren } from 'preact'
+import { useState, StateUpdater } from 'preact/hooks'
 
 interface ILockContext {
   isLock: boolean;
+  setIsLock: StateUpdater<boolean>| null
 }
 
-export const LockContext = createContext<ILockContext>({ isLock: false })
+export const LockContext = createContext<ILockContext>({ isLock: false, setIsLock: null })
+export const LockProvider = ({ children }: { children: ComponentChildren }): h.JSX.Element => {
+  const [isLock, setIsLock] = useState<boolean>(false)
+
+  return (
+    <LockContext.Provider value={{ isLock, setIsLock }} children={children} />
+  )
+}
